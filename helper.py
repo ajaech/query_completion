92a9c8d4a46e12e75d8e62bc5e9620d4e75e8c6e
Aaron Jaech <ajaech@cs.washington.edu>
2017-11-15 12:26:15 -0800

    remove dead code

--- helper.py
+++ helper.py
@@ -19,21 +19,6 @@ def GetParams(filename, mode, expdir):
   return params
 
 
-def haversine(lon1, lat1, lon2, lat2):
-    """
-    Calculate the great circle distance between two points 
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    km = 6367 * c
-    return km
-
 def levenshtein(source, target, cutoff=None):
     if len(source) < len(target):
         return levenshtein(target, source, cutoff=cutoff)
